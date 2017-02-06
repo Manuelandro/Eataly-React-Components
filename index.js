66965d2d1b5fd2335d5bd5acd9e5662a0b684918
Manuel Palma <manuelpalma@MacBook-Pro-di-Manuel.local>
2017-02-06 15:31:26 +0100

    first components

---
+++ index.js
@@ -0,0 +1,9 @@
+/* typos */
+export { default as H1 } from './lib/typo/H1'
+export { default as H1 } from './lib/typo/p'
+
+/* buttons */
+export { default as Button } from './lib/button'
+
+/* modal */
+export { default as Modal } from './lib/modal'
