bb8fa7c80aaa963e76bbfafb5c212df480705345
Manuel Palma <manuelpalma@MacBook-Pro-di-Manuel.local>
2017-02-07 11:10:21 +0100

    transpiling

--- index.js
+++
@@ -1,9 +0,0 @@
-/* typos */
-export { default as H1 } from './lib/typo/H1'
-export { default as P } from './lib/typo/P'
-
-/* buttons */
-export { default as Button } from './lib/button'
-
-/* modal */
-export { default as Modal } from './lib/modal'
